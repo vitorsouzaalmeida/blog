edef070475ff1db12143e0f53924f642cfade69d
Vitor S. Almeida <vi.souza.almeida@protonmail.com>
2026-06-02 08:56:14 -0300

    chore: format

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
