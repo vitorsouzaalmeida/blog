33c2fbf8539422463f12822a9938007130834cdc
Vitor S. Almeida <vi.souza.almeida@protonmail.com>
2026-09-10 15:30:12 -0300

    doc: intro

--- src/site.ts
+++ src/site.ts
@@ -2,9 +2,7 @@ export const site = {
   title: 'vitor s. almeida',
   author: 'vitor s. almeida',
   url: 'https://vitorsalmeida.com',
-  description:
-    'A dedicated space to share part of me. You will find some articles, essays and some links',
-  birthYear: 2004,
+  description: "Vitor's personal website",
   github: 'https://github.com/vitorsouzaalmeida/',
   linkedin: 'https://www.linkedin.com/in/vitorsalmeida/',
   source: 'https://github.com/vitorsouzaalmeida/blog',
